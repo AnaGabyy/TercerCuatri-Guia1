8fd35088d19f988c6a29e9b6e699ca8fbaa688a7
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-01 01:15:03 -0300

    Clase abstracta Empleado, con sus métodos

---
+++ TercerCuatri-Guia1/src/cuartoEjercicio/Empleado.java
@@ -0,0 +1,35 @@
+package cuartoEjercicio;
+
+public abstract class Empleado {
+
+    private TipoEmpleado tipoEmpleado;
+    private TipoBonificacion tipoBonificacion;
+
+    public Empleado(TipoEmpleado tipoEmpleado, TipoBonificacion tipoBonificacion) {
+        this.tipoEmpleado = tipoEmpleado;
+        this.tipoBonificacion = tipoBonificacion;
+    }
+
+    public TipoEmpleado getTipoEmpleado() {
+        return tipoEmpleado;
+    }
+
+    public void setTipoEmpleado(TipoEmpleado tipoEmpleado) {
+        this.tipoEmpleado = tipoEmpleado;
+    }
+
+    public TipoBonificacion getTipoBonificacion() {
+        return tipoBonificacion;
+    }
+
+    public void setTipoBonificacion(TipoBonificacion tipoBonificacion) {
+        this.tipoBonificacion = tipoBonificacion;
+    }
+
+    public abstract double calcularSalario();
+
+    public double calcularSalarioTotal(TipoEmpleado tipoEmpleado){
+        return calcularSalario();
+    }
+
+}
