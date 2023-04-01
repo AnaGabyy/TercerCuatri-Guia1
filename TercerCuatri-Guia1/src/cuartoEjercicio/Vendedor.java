1be55476943c3c6a43fb8a6147274e0f7d5e623b
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-01 01:16:29 -0300

    Clase Vendedor (extends Empleado)

---
+++ TercerCuatri-Guia1/src/cuartoEjercicio/Vendedor.java
@@ -0,0 +1,34 @@
+package cuartoEjercicio;
+
+public class Vendedor extends Empleado{
+
+    public Vendedor(TipoEmpleado tipoEmpleado, TipoBonificacion tipoBonificacion) {
+        super(tipoEmpleado, tipoBonificacion);
+    }
+
+    @Override
+    public double calcularSalario() {
+        double salario = 1000.0d;
+
+        if (TipoEmpleado.VENDEDOR.equals(getTipoEmpleado())) {
+
+            switch (getTipoBonificacion()){
+
+                case BONO:
+                    salario = salario + 1000.0d;
+                    break;
+
+                case CHEQUE:
+                    salario = salario + 900.0d;
+                    break;
+
+                case EXTRA:
+                    salario = salario + 300.0d;
+                    break;
+            }
+
+        }
+
+        return salario;
+    }
+}
