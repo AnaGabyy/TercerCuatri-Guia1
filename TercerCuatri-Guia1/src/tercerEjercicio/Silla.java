8f258ada9e9b0afd27e733bfdf9557c749fc272e
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-03-31 22:42:57 -0300

    Clase Silla (extends Mueble)

---
+++ TercerCuatri-Guia1/src/tercerEjercicio/Silla.java
@@ -0,0 +1,28 @@
+package tercerEjercicio;
+
+public class Silla extends Mueble{
+
+    public Silla(TipoMaterial tipoMaterial) {
+        super(tipoMaterial);
+    }
+
+    @Override
+    public String calcularPrecio() {
+
+        double precio = 0.0d;
+
+        if (TipoMaterial.MADERA.equals(getTipoMaterial())){
+            precio = 4000.0d;
+
+        } else if (TipoMaterial.PLASTICO.equals(getTipoMaterial())) {
+            precio = 2000.0d;
+
+        } else if (TipoMaterial.HIERRO.equals(getTipoMaterial())) {
+            precio = 6000.0d;
+
+        }
+
+        return "La silla de " + getTipoMaterial() + " tiene un precio de " +  precio + " $";
+
+    }
+}
