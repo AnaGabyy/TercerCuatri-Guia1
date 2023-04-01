aa6e3564129393b99a98dd575b222de943081adb
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-03-31 22:13:47 -0300

    Prueba con objetos: coche, moto , camion

--- TercerCuatri-Guia1/src/segundoEjercicioTest/Main.java
+++ TercerCuatri-Guia1/src/segundoEjercicioTest/Main.java
@@ -1,5 +1,10 @@
 package segundoEjercicioTest;
 
+import segundoEjercicio.Camion;
+import segundoEjercicio.Coche;
+import segundoEjercicio.Moto;
+import segundoEjercicio.TipoCombustible;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,6 +14,16 @@ public class Main {
         "TipoCombustible" para definir los tipos de combustible que utilizan los vehículos y un método
         "arrancarVehiculo(TipoCombustible)" que utiliza polimorfismo para llamar al método "arrancar()"
         en cada uno de ellos, dependiendo del tipo de combustible utilizado.*/
+
+        Coche coche = new Coche(TipoCombustible.GAS);
+        System.out.println(coche.arrancarVehiculo(coche.getTipoCombustible()));
+
+        Moto moto = new Moto(TipoCombustible.NAFTA);
+        System.out.println(moto.arrancarVehiculo(moto.getTipoCombustible()));
+
+        Camion camion = new Camion(TipoCombustible.DIESEL);
+        System.out.println(camion.arrancarVehiculo(camion.getTipoCombustible()));
+
 
     }
 }
