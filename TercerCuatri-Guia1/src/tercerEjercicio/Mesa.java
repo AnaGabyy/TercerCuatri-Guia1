a91372895a78d66d536a55e84a2bb3606dda497a
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-03-31 22:43:14 -0300

    Clase Mesa (extends Mueble)

---
+++ TercerCuatri-Guia1/src/tercerEjercicio/Mesa.java
@@ -0,0 +1,30 @@
+package tercerEjercicio;
+
+public class Mesa extends Mueble{
+
+    public Mesa(TipoMaterial tipoMaterial) {
+        super(tipoMaterial);
+    }
+
+    @Override
+    public String calcularPrecio() {
+
+        double precio = 0.0d;
+
+        if (TipoMaterial.MADERA.equals(getTipoMaterial())){
+            precio = 5000.0d;
+
+        } else if (TipoMaterial.PLASTICO.equals(getTipoMaterial())) {
+            precio = 2500.0d;
+
+        } else if (TipoMaterial.HIERRO.equals(getTipoMaterial())) {
+            precio = 7000.0d;
+
+        }
+
+        return "La mesa de " + getTipoMaterial() + " tiene un precio de " +  precio + " $";
+
+    }
+
+
+}
