c92c72985c993b1ee8a00bc9978712508f035712
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-04-01 17:31:07 -0300

    Clase abstracta Empleado
    Con sus atributos, métodos, constructor

---
+++ TercerCuatri-Guia1/src/primerEjercicio/Empleado.java
@@ -0,0 +1,33 @@
+package primerEjercicio;
+
+import java.util.Date;
+
+public abstract class Empleado {
+
+    private String nombre;
+    private String fechaNacimiento;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
